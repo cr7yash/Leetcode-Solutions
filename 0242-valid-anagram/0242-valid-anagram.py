1f08488f5ad45cce13f4278257e9a47507a16a25
Yash Srivastava <yashaqua@gmail.com>
2026-05-14 06:58:50 +0530

    Time: 11 ms (76.37%), Space: 19.6 MB (27.27%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        map1 = {}
+        map2 = {}
+
+        for i in s:
+            map1[i] = map1.get(i,0) + 1
+
+        for j in t:
+            map2[j] = map2.get(j,0) + 1
+
+
+        if map1 == map2:
+            return True
+
+        return False
