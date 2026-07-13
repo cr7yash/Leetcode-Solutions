1ffef78a868938f9d97338cd9eac4006e4cda6ff
Yash Srivastava <yashaqua@gmail.com>
2026-07-12 18:20:15 -0700

    Time: 4 ms (41.71%), Space: 20.4 MB (58.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for key,val in enumerate(nums):
+            compliment = target - val
+            if compliment in seen:
+                return [seen[compliment], key]
+            seen[val] = key
+
+        
