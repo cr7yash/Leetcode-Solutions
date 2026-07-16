401fb0db109439af0db1262d4a7a17eddfdc48b9
Yash Srivastava <yashaqua@gmail.com>
2026-07-16 08:50:01 -0700

    Time: 34 ms (13.36%), Space: 37.3 MB (6.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,15 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-
-        freq = {}
+        
+        count = {}
 
         for num in nums:
-            # Get current count (default to 0 if not found) and add 1
-            freq[num] = freq.get(num, 0) + 1
-            
-            # Check immediately if this number has appeared more than once
-            if freq[num] > 1:
-                return True
+            count[num] = count.get(num,0) + 1
 
-        return False
+            if count[num] > 1:
+                return True
         
+        return False
