084bb1a6c62645d8044c01b224a2106ef449da5f
Yash Srivastava <yashaqua@gmail.com>
2026-09-18 14:50:51 -0700

    Time: 26 ms (42.29%), Space: 25.6 MB (54.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,14 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        length = len(nums)
-        answer = [0] * length
-        
-        # 1. Fill answer with Left products
-        answer[0] = 1
-        for i in range(1, length):
-            answer[i] = answer[i-1] * nums[i-1]
-        
-        # 2. Multiply by Right products on the fly
-        R = 1
-        for i in range(length - 1, -1, -1):
-            # Multiply the Left product (already in answer) by the current Right product
-            answer[i] = answer[i] * R
-            
-            # Update the Right product for the next step
-            R = R * nums[i]
-            
-        return answer
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        n = len((nums))
+        output = [1] * n
+        right = 1
+        for i in range(1,n):
+            output[i]  = output[i-1] * nums[i-1]
+
+        for i in range(n-1,-1,-1):
+            output[i] = output[i] * right
+
+            right = right * nums[i]
+
+        return output 
